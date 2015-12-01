cacd72b2354178a1b8d248e692d7f9e5af750ac4
Sukhjinder Nahal <nahal.snahal@gmail.com>
2015-12-01 09:29:10 -0500

    Changed Layout- added prev, next buttons. Added ability to move between markers using next and prev buttons

--- app/src/main/java/com/location/locationapp/MapsActivity.java
+++ app/src/main/java/com/location/locationapp/MapsActivity.java
@@ -5,11 +5,10 @@ import android.location.Address;
 import android.location.Geocoder;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.EditText;
-import android.widget.LinearLayout;
-
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
@@ -17,23 +16,21 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
-
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-
 import io.realm.*;
-import io.realm.annotations.Ignore;
-import io.realm.annotations.PrimaryKey;
+
 
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
 
-    ArrayList<String> mMarkers = new ArrayList<>();
+//    ArrayList<String> mMarkers = new ArrayList<>();
 
+    private ArrayList<Marker> Markers= new ArrayList<>();// Holds a list of Markers
+    private static int counter = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -58,12 +55,13 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
+        mMap.setMapType(GoogleMap.MAP_TYPE_HYBRID);
 
-        // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).draggable(true).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
-        mMap.setMyLocationEnabled(true);
+//        // Add a marker in Sydney and move the camera
+//        LatLng sydney = new LatLng(-34, 151);
+//        mMap.addMarker(new MarkerOptions().position(sydney).draggable(false).title("Marker in Sydney"));
+//        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+//        mMap.setMyLocationEnabled(true);
 
         Realm realm = Realm.getInstance(this); //Initializes Realm
         RealmResults<Location> result = realm.where(Location.class).findAll();
@@ -74,12 +72,13 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
                 String locationName = result.get(i).getName();
 
-                MarkerOptions mo = new MarkerOptions().position(tempLatLng).title(locationName + ": " + i);
+                MarkerOptions mo = new MarkerOptions().position(tempLatLng).title(result.get(i).getName());
 
                 mMap.addMarker(mo);
                 Marker mkr = mMap.addMarker(mo);
 
-                mMarkers.add(mkr.getId());
+//                mMarkers.add(mkr.getId());
+                Markers.add(mkr); //Adds Marker to ArrayList
             }
         }
 
@@ -100,9 +99,11 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     {
 
         EditText search = (EditText)findViewById(R.id.map_search);
-        LinearLayout titleLayout = (LinearLayout)findViewById(R.id.title_layout);
+//        LinearLayout titleLayout = (LinearLayout)findViewById(R.id.title_layout);
+        EditText setTitle = (EditText)findViewById(R.id.map_title);
+        String title = setTitle.getText().toString();
 
-        titleLayout.setVisibility(view.VISIBLE); //If you click search, the EditText and Buttons to change the title become visible. 
+//        titleLayout.setVisibility(view.VISIBLE); //If you click search, the EditText and Buttons to change the title become visible.
 
         String location = search.getText().toString();
         hideKeyboard(this);
@@ -120,18 +121,19 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
             if(addressList.get(0)!=null)
             {
 
-                for(int j = 0; j < mMarkers.size(); j++){
-                    System.out.println(mMarkers.get(j));
-                }
+//                for(int j = 0; j < mMarkers.size(); j++){
+//                    System.out.println(mMarkers.get(j));
+//                }
 
                 LatLng latlng = new LatLng(addressList.get(0).getLatitude(),addressList.get(0).getLongitude());
 
-                MarkerOptions mo = new MarkerOptions().position(latlng).draggable(true).title(location);
+                MarkerOptions mo = new MarkerOptions().position(latlng).draggable(false).title(title);
 
                 mMap.addMarker(mo);
                 Marker mkr = mMap.addMarker(mo);
 
-                mMarkers.add(mkr.getId());
+//                mMarkers.add(mkr.getId());
+                Markers.add(mkr);
 
                 mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latlng,15));  //20 is the zoom level
 
@@ -140,7 +142,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                 Location loc = new Location(); //Initalizes the Location class
                 loc.setLatitude(addressList.get(0).getLatitude()); //Sets the lat
                 loc.setLongitude(addressList.get(0).getLongitude()); //Sets the long
-                loc.setName(location); //Sets the name
+                loc.setName(title); //Sets the name
                 loc.setPK((int)addressList.get(0).getLatitude() + (int)addressList.get(0).getLongitude()); //Sets the PK (Might want to make this less bad)
                 realm.beginTransaction(); //Starts the save
                 realm.copyToRealmOrUpdate(loc); //Saves
@@ -153,22 +155,17 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
     }
 
-    public void onTitle(View view)
-    {
-
-        //Need to figure out how to actually grab a marker by its ID, which we have saved in mMarkers.
-        EditText setTitle = (EditText)findViewById(R.id.map_title);
-        LinearLayout titleLayout = (LinearLayout)findViewById(R.id.title_layout);
-
-        String markerID = mMarkers.get(mMarkers.size()-1);
-        //Now the string MarkerID is the ID we want, but I don't know how to actually grab the marker this is attached to...
-
-
-
-
-
-
-    }
+//    public void onTitle(View view)
+//    {
+//
+//        //Need to figure out how to actually grab a marker by its ID, which we have saved in mMarkers.
+//        EditText setTitle = (EditText)findViewById(R.id.map_title);
+//        LinearLayout titleLayout = (LinearLayout)findViewById(R.id.title_layout);
+//
+//        String markerID = mMarkers.get(mMarkers.size()-1);
+//        //Now the string MarkerID is the ID we want, but I don't know how to actually grab the marker this is attached to...
+//
+//    }
 
 
     public static void hideKeyboard(Activity activity)
@@ -176,4 +173,29 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         InputMethodManager inputMethodManager = (InputMethodManager)  activity.getSystemService(Activity.INPUT_METHOD_SERVICE);
         inputMethodManager.hideSoftInputFromWindow(activity.getCurrentFocus().getWindowToken(), 0);
     }
+
+//  Moves the screen to the next Marker location
+    public void Next(View view){
+
+        if(counter==Markers.size())
+        {
+            counter=0;
+        }
+            Marker mkr;
+            mkr=Markers.get(counter++);
+            mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(mkr.getPosition(),15));  //20 is the zoom level
+    }
+
+// Moves the screen to the previous Marker location
+    public void Previous(View view)
+    {
+        if(counter==0){
+            counter=Markers.size();
+        }
+        Marker mkr;
+        counter--;
+        mkr=Markers.get(counter);
+        mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(mkr.getPosition(),15));  //20 is the zoom level
+    }
+
 }
